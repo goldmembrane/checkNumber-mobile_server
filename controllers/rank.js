62c43e4bb0e6818c3fd858169cb66e482ba27e8e
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-03-31 19:41:12 +0900

    create rank controller and make post, get method

---
+++ controllers/rank.js
@@ -0,0 +1,34 @@
+const { Rank }  = require('../models');
+
+module.exports = {
+    post: (req, res) => 
+    {
+        let username = req.body.username;
+        let score = req.body.score;
+
+        Rank.create({
+            username: username,
+            score: score,
+        })
+        .then((data) => {
+            res.status(201).json({message: 'Save Success'}).end();
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(502).send(error);
+        });
+    },
+    get: (req, res) => {
+        Rank.findAll({
+            limit: 8,
+            order: [['score', 'DESC']],
+        })
+        .then((data) => {
+            res.status(200).json(data).end();
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(501).send(error);
+        });
+    },
+}
